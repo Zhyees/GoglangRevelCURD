4df7ac40dcc05e17845552bd8db6d93f011d34d5
unknown <khamdan.ngazis@gmail.com>
2017-12-12 17:19:45 +0700

    create view data

---
+++ app/viewmodels/employedvm/getallviewmodels.go
@@ -0,0 +1,28 @@
+package employedvm
+
+import (
+	"github.com/go-gorp/gorp"
+	"github.com/revel/revel"
+	"GoglangRevelCURD/app/models"
+)
+
+type GetAllViewModels struct {
+	Txn *gorp.Transaction
+}
+
+type GetAllViewModelsResponse struct {
+	EmlpoyedList []interface{} `json:"employed"` 
+}
+
+func (vm GetAllViewModels) LoadAll() GetAllViewModelsResponse{
+	employedList, err := vm.Txn.Select(models.Employed{}, "SELECT * FROM `data`")
+	if err == nil{
+		return GetAllViewModelsResponse{
+			EmlpoyedList: employedList,
+		}
+	}
+
+	revel.WARN.Printf("%#v", err)
+
+	return GetAllViewModelsResponse{}
+}
